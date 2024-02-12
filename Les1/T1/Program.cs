b60b1cb6a2eb7dd7d887b83d906b5995263c9c23
Grogo <n89000547345@yandex.ru>
2024-02-12 17:28:57 +0700

    add different value

--- Les1/T1/Program.cs
+++ Les1/T1/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello world!");
+﻿// Console.WriteLine("Hello world!");
+
+int number1 = 100;
+Console.WriteLine(number1);
+double d = 3.21;
+char c = '!';
+bool b = false;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
